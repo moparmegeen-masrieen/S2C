1c3532711250e37c111cb160a8548dba2dc01b93
Zeiad-Abozeid <42680010+Zeiad-Abozeid@users.noreply.github.com>
2018-08-31 13:05:05 -0700

    Update Core1.py

--- Core1.py
+++ Core1.py
@@ -1,2 +1,12 @@
+nor = ['command','stuff','this','is','Just','a','sample']
 def main():
+    # Algrithm to count the percent of being wrong in the calculations of the code
+    x_dot = 10
+    x = len(nor)
+    delta_x = |x_dot - x|
+    r = delta_x / x_dot * 100
+    print('this is the wrong percetage', r)
     
+#Created by Zeiad Hosni!!!!1
+    
+        
